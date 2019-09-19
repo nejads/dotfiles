fd52eb7e65073f4d3f46acd50dc125b3ecb755d8
Soroush Nejad <sardabir@gmail.com>
2019-09-19 21:21:04 +0200

    Fixed boostrap sctipt

--- bootstrap.sh
+++ bootstrap.sh
@@ -332,7 +332,16 @@ function install_epubee() {
 }
 
 function mackup_restore() {
-
+    info "Mackup using iCloud to restore your settings. Have you logged in iCloud?"
+    select result in Yes No
+    do
+        if [ "$result" = "Yes" ]; then
+            run "Mackup restore" "mackup restore --force"
+            break;
+        else
+            error "Please login to your icloud drive first, then choose Yes"
+        fi
+    done
 }
 
 ################################

d745479c5d9398fe1119ffce5f4d677b88238861
Soroush Nejad <sardabir@gmail.com>
2019-10-10 16:42:47 +0200

    Changed SSID name

--- hammerspoon/scripts/set_volume_outside_home.lua
+++ hammerspoon/scripts/set_volume_outside_home.lua
@@ -1,5 +1,5 @@
 wifiwatcher = hs.wifi.watcher.new(function ()
-    homeSSID = "EnDuvaSattPaEnGren"
+    homeSSID = "Saturn-5G"
     currentSSID = hs.wifi.currentNetwork()
     if currentSSID==homeSSID then
         hs.audiodevice.defaultOutputDevice():setVolume(25)
